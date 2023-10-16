3c8d60b072378d32ef3414884dee3f637e4f0b96
Heattehnik <gadjka@mail.ru>
2023-10-16 19:41:12 +0500

    Validation

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
-from fastapi import FastAPI
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import ValidationException
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="Trading app"
 )
 
+
+@app.exception_handler(ValidationException)
+async def validation_exception_handler(request: Request, exc: ValidationException):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()})
+    )
+
+
 fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 1, "role": "admin", "name": ["Bob"]},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "trader", "name": "Kolya", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"},
+    ]
+     },
 ]
 
 
-@app.get("/users/{user_id}")
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
@@ -23,9 +61,24 @@ fake_trades = [
  ]
 
 
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float = Field(ge=0)
+
+
 @app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):
+def get_trades(limit: int = 10, offset: int = 0):
     return fake_trades[offset:][:limit]
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
 
 
 fake_users2 = [
